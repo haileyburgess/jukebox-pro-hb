f9682b170d63be76b7223410e50775a3cf48130f
Hailey Burgess <hailey.burgess@redcross.org>
2025-07-07 21:20:47 -0400

    completed routes but tests arent all passing yet

--- api/playlists.js
+++ api/playlists.js
@@ -5,32 +5,32 @@ export default router;
 import {
   createPlaylist,
   getPlaylistById,
-  getPlaylists,
+  getPlaylistByUserId,
 } from "#db/queries/playlists";
 import { createPlaylistTrack } from "#db/queries/playlists_tracks";
 import { getTracksByPlaylistId } from "#db/queries/tracks";
+import requireUser from "#middleware/requireUser";
+import requireBody from "#middleware/requireBody";
+
+router.use(requireUser);
 
 router
   .route("/")
   .get(async (req, res) => {
-    const playlists = await getPlaylists();
+    const playlists = await getPlaylistByUserId(req.user.id);
     res.send(playlists);
   })
-  .post(async (req, res) => {
-    if (!req.body) return res.status(400).send("Request body is required.");
-
+  .post(requireBody(["name", "description"]), async (req, res) => {
     const { name, description } = req.body;
-    if (!name || !description)
-      return res.status(400).send("Request body requires: name, description");
-
-    const playlist = await createPlaylist(name, description);
+    const playlist = await createPlaylist(name, description, req.user.id);
     res.status(201).send(playlist);
   });
 
 router.param("id", async (req, res, next, id) => {
   const playlist = await getPlaylistById(id);
   if (!playlist) return res.status(404).send("Playlist not found.");
-
+  if (playlist.user_id !== req.user.id)
+    return res.status(403).send("no access to this playlist");
   req.playlist = playlist;
   next();
 });
@@ -46,11 +46,7 @@ router
     res.send(tracks);
   })
   .post(async (req, res) => {
-    if (!req.body) return res.status(400).send("Request body is required.");
-
     const { trackId } = req.body;
-    if (!trackId) return res.status(400).send("Request body requires: trackId");
-
     const playlistTrack = await createPlaylistTrack(req.playlist.id, trackId);
     res.status(201).send(playlistTrack);
   });
--- api/tracks.js
+++ api/tracks.js
@@ -3,14 +3,28 @@ const router = express.Router();
 export default router;
 
 import { getTracks, getTrackById } from "#db/queries/tracks";
+import { getPlaylistsByTrackId } from "#db/queries/playlists";
+import requireUser from "#middleware/requireUser";
+
+router.use(requireUser);
 
 router.route("/").get(async (req, res) => {
   const tracks = await getTracks();
   res.send(tracks);
 });
 
-router.route("/:id").get(async (req, res) => {
-  const track = await getTrackById(req.params.id);
-  if (!track) return res.status(404).send("Track not found.");
-  res.send(track);
+router.param("id", async (req, res) => {
+  const track = await getTrackById(id);
+  if (!track) return res.status(404).send("track not found");
+  req.track = track;
+  next();
+});
+
+router.route("/:id").get((req, res) => {
+  res.send(req.track);
+});
+
+router.route("/:id/playlists").get(requireUser, async (req, res) => {
+  const playlists = await getPlaylistsByTrackId(req.track.id);
+  res.send(playlists);
 });
